be00dd5eb66c9e74f169c19f2d8467b5d0af8e03
Dagger <odidihope@gmail.com>
2025-02-01 17:13:47 +0100

    fixed

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,1 @@
-export const BASE_API_URL = "http://localhost:8080";
-// export const BASE_API_URL = "https://backend-repo-1-x2b1.onrender.com";
+export const BASE_API_URL = "https://backend-repo-1-x2b1.onrender.com";
